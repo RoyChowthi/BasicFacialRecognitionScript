cd745db73770c1473ce9a89e884de6846f75adbd
RoyChowthi <70399240+RoyChowthi@users.noreply.github.com>
2022-02-09 14:58:45 -0500

    Add files via upload

---
+++ FaceRecognitionScriptFolder/BasicFaceRecognitionScript.py
@@ -0,0 +1,14 @@
+import face_recognition
+
+image_of_known_person = face_recognition.load_image_file('C:\\Users\\royrw\\Desktop\\Coding stuff\\known\\BILLGATES.jpg')
+face_encoding = face_recognition.face_encodings(image_of_known_person)[0]
+
+unknown_image = face_recognition.load_image_file('C:\\Users\\royrw\\Desktop\\Coding stuff\\unknown\\random_3.jpg')
+unknown_face_encoding = face_recognition.face_encodings(unknown_image)[0]
+
+results = face_recognition.compare_faces([face_encoding], unknown_face_encoding,tolerance=0.5)
+
+if results[0]:
+    print('These are the same people')
+else:
+    print('These are not the same people')
