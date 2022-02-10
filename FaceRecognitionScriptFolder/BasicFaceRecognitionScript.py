edf9c6e5f27ad570a05db79d4da1d1aadcb69a7a
RoyChowthi <70399240+RoyChowthi@users.noreply.github.com>
2022-02-09 23:12:50 -0500

    Add files via upload
    there was an error with the code now it's fixed.

--- FaceRecognitionScriptFolder/BasicFaceRecognitionScript.py
+++ FaceRecognitionScriptFolder/BasicFaceRecognitionScript.py
@@ -1,9 +1,9 @@
 import face_recognition
 
-image_of_known_person = face_recognition.load_image_file('C:\\Users\\royrw\\Desktop\\Coding stuff\\known\\BILLGATES.jpg')
+image_of_known_person = face_recognition.load_image_file('known\\BILLGATES.jpg')
 face_encoding = face_recognition.face_encodings(image_of_known_person)[0]
 
-unknown_image = face_recognition.load_image_file('C:\\Users\\royrw\\Desktop\\Coding stuff\\unknown\\random_3.jpg')
+unknown_image = face_recognition.load_image_file('unknown\\random_3.jpg')
 unknown_face_encoding = face_recognition.face_encodings(unknown_image)[0]
 
 results = face_recognition.compare_faces([face_encoding], unknown_face_encoding,tolerance=0.5)
